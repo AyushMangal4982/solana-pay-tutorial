a2e1f059748df4726f9b0215d4135ff846442764
AyushMangal4982 <mangal.ayush.4982@gmail.com>
2022-04-08 13:25:11 +0530

    first commit

---
+++ lib/addresses.ts
@@ -0,0 +1,12 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('Hce3QRsCidLkbE1124oWwJEeAroToZWyhFJuohrusYWB');
+
+export const usdcAddress = new PublicKey(
+  'Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr'
+)
+
+export const couponAddress = new PublicKey(
+  '8K9Q63MCq5kF1j7T6SRCCCJdXswSv9ATY3JvSMEojGxj'
+)
